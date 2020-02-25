774cdd0eb19054d7e90c88d3ed05adfee4284290
Alex <alex@nder.one>
2020-02-25 14:44:51 +0200

    New challenge

---
+++ level 4/distract-the-guards.py
@@ -0,0 +1,7 @@
+def solution(banana_list):
+    return
+
+
+if __name__ == '__main__':
+    print('pass' if solution([1, 1]) == 2 else 'fail')
+    print('pass' if solution([1, 7, 3, 21, 13, 19]) == 0 else 'fail')
