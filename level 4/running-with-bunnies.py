97fb5b41cbf449c2ecd3d64e64f64227a43b9006
Alex <28705249+alexcs123@users.noreply.github.com>
2020-03-05 00:15:37 +0200

    New challenge

---
+++ level 4/running-with-bunnies.py
@@ -0,0 +1,7 @@
+def solution(times, time_limit):
+    return
+
+
+if __name__ == '__main__':
+    print('pass' if solution([[0, 2, 2, 2, -1], [9, 0, 2, 2, -1], [9, 3, 0, 2, -1], [9, 3, 2, 0, -1], [9, 3, 2, 2, 0]], 1) == [1, 2] else 'fail')
+    print('pass' if solution([[0, 1, 1, 1, 1], [1, 0, 1, 1, 1], [1, 1, 0, 1, 1], [1, 1, 1, 0, 1], [1, 1, 1, 1, 0]], 3) == [0, 1] else 'fail')
